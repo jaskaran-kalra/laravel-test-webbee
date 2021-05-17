e14c4426e6c7c2942da3bb089ce2f802ed98da30
Jaskaran Singh Kalra <wp.jaskarankalra@gmail.com>
2021-05-17 16:57:52 +0530

    coding task 2 complete

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Event;
+use Carbon\Carbon;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -176,6 +177,8 @@ class EventsController extends BaseController
      */
 
     public function getFutureEventsWithWorkshops() {
-        throw new \Exception('implement in coding task 2');
+        return Event::whereHas('workshops', function($query) {
+            $query->whereDate('start','>', Carbon::now());
+        })->with('workshops')->get();
     }
 }
